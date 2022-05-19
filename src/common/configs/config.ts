6439d86a654f246f6ac8f182ebb1587fac137ffa
bibekgupta3333 <bibekgupta3333@gmail.com>
2022-05-20 00:21:32 +0545

    ✨ FIX #3 adding configs interface

---
+++ src/common/configs/config.ts
@@ -0,0 +1,31 @@
+// interface
+import type { Config } from './config.interface';
+
+const config: Config = {
+  nest: {
+    port: 3000,
+  },
+  cors: {
+    enabled: true,
+  },
+  swagger: {
+    enabled: true,
+    title: 'Nestjs Chat API',
+    description: 'The Chat API description',
+    version: '1.5',
+    path: 'api',
+  },
+  graphql: {
+    playgroundEnabled: true,
+    debug: true,
+    schemaDestination: './src/schema.graphql',
+    sortSchema: true,
+  },
+  security: {
+    expiresIn: '60d',
+    refreshIn: '30d',
+    bcryptSaltOrRound: 10,
+  },
+};
+
+export default (): Config => config;
